ea545fee22012ae7854a1cf283316ae34740fcd5
Timothy Claassens <tim.claassens@rabobank.nl>
2017-12-09 02:22:15 +0100

    Why is this always such a hassle

--- deployments/deploy_prod.sh
+++ deployments/deploy_prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rsync -arve "ssh $SSH_OPTIONS" --no-perms --no-owner --no-group --delete --exclude '.git' ../i-love-hue-two-server/dist/server/ root@$DEPLOY_HOST:/var/i-love-hue-two-server/
-rsync -arve "ssh $SSH_OPTIONS" --no-perms --no-owner --no-group --delete --exclude '.git' ../i-love-hue-two-client/dist/ root@$DEPLOY_HOST:/var/www/html/i-love-hue-two
+rsync -arve "ssh $SSH_OPTIONS" --no-perms --no-owner --no-group --delete --exclude '.git' i-love-hue-two-server/dist/server/ root@$DEPLOY_HOST:/var/i-love-hue-two-server/
+rsync -arve "ssh $SSH_OPTIONS" --no-perms --no-owner --no-group --delete --exclude '.git' i-love-hue-two-client/dist/ root@$DEPLOY_HOST:/var/www/html/i-love-hue-two
